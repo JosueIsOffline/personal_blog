b142fd729807e993c0c8ff4ab396cd2a3b168862
JosueIsOffline <josuehernandez2314@gmail.com>
2023-11-24 22:29:43 -0400

    port

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # personal_blog
  
+test
--- server/src/app.js
+++ server/src/app.js
@@ -1,13 +1,15 @@
 // Import necessary modules
 const Express = require('express')
+const chalk = require('chalk')
 require('dotenv').config()
 
 
 const app = Express()
 const port = process.env.PORT || 5000
 
+
 app.listen(port, () => {
-    console.log(`http://localhost:${port}`)
+    console.log(chalk.hex('#092664')`http://localhost:${port}`)
 })
 
 
