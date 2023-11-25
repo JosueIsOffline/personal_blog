1d9212e77ca51b032dbf7bacd9aa186502606e79
JosueIsOffline <josuehernandez2314@gmail.com>
2023-11-24 22:49:47 -0400

    routes

--- server/src/app.js
+++ server/src/app.js
@@ -8,8 +8,13 @@ const app = Express()
 const port = process.env.PORT || 5000
 
 
+app.use('/', (req, res) => {
+    res.send('<h1>GoodBye World!</h1>')
+})
+
+
 app.listen(port, () => {
-    console.log(chalk.hex('#092664')`http://localhost:${port}`)
+    console.log(chalk.hex('#000fff')`http://localhost:${port}`)
 })
 
 
